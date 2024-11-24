77027156b82b91efaa7dce841945b47acc15ba93
Rasmus <rubikscubr@mail.de>
2024-11-24 17:37:53 +0100

    schöner

--- flutttest/lib/widgets/groupselect.dart
+++ flutttest/lib/widgets/groupselect.dart
@@ -4,22 +4,33 @@ import 'creategroup.dart';
 class GroupSelect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary,);
+    TextStyle(color: theme.colorScheme.onPrimary, fontSize: 20);
+
     return Scaffold(
       appBar: AppBar(title: const Text("Wähle Gruppe")),
-      body: Column(
-        children: [
-          ElevatedButton(
-            child: Text("+"),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CreateGroup(),
-                ),
-              );
-            },
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: theme.colorScheme.primary, // Set the background color
+                minimumSize: Size(200, 60),  // width: 200, height: 60
+              ),
+              child: Text("+", style: style),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CreateGroup(),
+                  ),
+                );
+              },
+            )
+          ]  
+        )  
       ),
     );
   }
